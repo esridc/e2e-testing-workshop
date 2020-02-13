44c2a3d47bf8f5b90576653a0fce6edbb683336c
Andrew Tate <drewctate@gmail.com>
2020-02-13 10:56:28 -0700

    Google test

---
+++ 2_google_test/test/specs/google-search.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+
+const SELECTORS = {
+    SEARCH_BAR: 'input[title="Search"]',
+    SEARCH_BUTTON: 'input[value="Google Search"]',
+    ORGANIC_LISTING_LINKS: '#search .g a',
+    ORGANIC_LISTING_HEADERS: '#search .g a h3',
+};
+
+const KEYS = {
+    ENTER: '\uE007'
+};
+
+describe('Esri DevSummit Google Search', () => {
+    it('should display correct titles for the first three listings', () => {
+        const url = 'https://google.com';
+        const searchText = 'esri devsummit workshops';
+
+        browser.url(url);
+
+        const searchBarEl = $(SELECTORS.SEARCH_BAR);
+        searchBarEl.waitForDisplayed();
+        searchBarEl.setValue(searchText);
+        browser.keys(KEYS.ENTER); // "Press" enter to search
+
+        const listingHeaderTexts = $$(SELECTORS.ORGANIC_LISTING_HEADERS).map(el => el.getText());
+
+        assert.equal(listingHeaderTexts[0], 'Pre-Summit Hands-On Training | 2020 Esri Developer Summit', 'First listing has correct title');
+        assert.equal(listingHeaderTexts[1], 'Agenda | 2020 Esri Developer Summit', 'Second listing has correct title');
+        assert.equal(listingHeaderTexts[2], '2019 Esri Developer Summit: January 31, Washington, D.C.', 'Third listing has correct title');
+
+        const firstListingHeaderLink = $(SELECTORS.ORGANIC_LISTING_LINKS);
+        firstListingHeaderLink.click();
+
+        assert.equal(browser.getUrl(), 'https://www.esri.com/en-us/about/events/devsummit/agenda/pre-summit-hands-on-training', 'Dev summit training page has correct URL');
+    });
+});
